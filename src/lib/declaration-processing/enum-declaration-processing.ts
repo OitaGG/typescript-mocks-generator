fb5a41e0a9a3cb323a818d62faa94827b7904219
Ilintsev <iilintsev@naumen.ru>
2023-10-12 14:31:28 +0300

    Add readme, use prettier to beatify code

--- src/lib/declaration-processing/enum-declaration-processing.ts
+++ src/lib/declaration-processing/enum-declaration-processing.ts
@@ -1,45 +1,19 @@
-import ts from 'typescript';
-
-import { Types } from '@root/types';
-
+import { CommonDeclarationProcessingParams } from '@lib/declaration-processing/types';
 import { processEnumProperty } from '@lib/property-processing/process-enum-property-type';
 
 /**
- * Параметры хелпера обработки определения типов
+ * Обработать определение Enum, встретившееся нам в файле
  */
-type EnumDeclarationProcessingParams = {
-  /**
-   * Наименование определения типа
-   */
-  aliasName: string;
-  /**
-   * Узел AST-дерева файла
-   */
-  node: ts.Node;
-  /**
-   * Ссылки на интерфейсы и типы из AST дерева файла
-   */
-  types: Types;
-  /**
-   * AST-дерево файла
-   */
-  sourceFile: ts.SourceFile;
-  /**
-   * Аккумулятор значений, полученных в результате обработки определения типов
-   */
-  accumulator: Record<string, any>;
-};
-
 export const enumDeclarationProcessing = ({
   accumulator,
   sourceFile,
   aliasName,
   types,
   node,
-}: EnumDeclarationProcessingParams) => {
+}: CommonDeclarationProcessingParams) => {
   const cache = {};
 
-  processEnumProperty({ accumulator: cache, propertyName: 'enum', types, typeName: aliasName });
+  processEnumProperty({ accumulator: cache, propertyName: 'enum', types, typeName: aliasName! });
 
   console.log(cache);
 };
